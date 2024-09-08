989d52ef6de94716fbd2614b34c2128feb7a94d0
imabari <imabari@gmail.com>
2024-09-08 10:28:10 +0900

    音声自動生成

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,68 @@
 import datetime
+import pathlib
+import re
 
 import pandas as pd
 import requests
+from gtts import gTTS
 from streamlit_folium import st_folium
 
 import folium
 import streamlit as st
 
 base_url = st.secrets["url"]
+
+
+def convert_time_format(time_str):
+    # 各時間帯を分割
+    time_ranges = time_str.split(" / ")
+    converted_ranges = []
+
+    for time_range in time_ranges:
+        # 正規表現で時間を抽出
+        m = re.match(r"(\d{2}):(\d{2})～(翌)?(\d{2}):(\d{2})", time_range)
+        if m:
+            start_hour, start_minute, next_day, end_hour, end_minute = m.groups()
+            # 分が0の場合のフォーマットを変換
+            start_time = f"{int(start_hour)}時" if start_minute == "00" else f"{int(start_hour)}時{int(start_minute)}分"
+            end_time = f"{int(end_hour)}時" if end_minute == "00" else f"{int(end_hour)}時{int(end_minute)}分"
+            # 翌日の場合のフォーマットを変換
+            if next_day:
+                converted_ranges.append(f"{start_time}から翌日{end_time}まで")
+            else:
+                converted_ranges.append(f"{start_time}から{end_time}まで")
+
+    # 変換された時間帯を結合
+    return "、".join(converted_ranges)
+
+
+def make_voice(current, fn):
+    date_string = re.sub(r"\d{4}年0?(\d+)月0?(\d+)日", r"\1月\2日", current["date_week"])
+
+    text = []
+
+    text.append(f"{date_string}の救急当番病院をおしらせします")
+
+    for hospital in current["hospitals"]:
+        match hospital["type"]:
+            case 6 | 7:
+                text.append(f'{hospital["medical"]}の診察は')
+            case 9:
+                text.append("島しょ部の診察は")
+
+        text.append(convert_time_format(hospital["time"]))
+        text.append(hospital["hira_address"])
+        text.append(hospital["hira_name"])
+        text.append("電話")
+        text.append(f'{hospital["daytime"]}')
+
+    message = "\n".join(text)
+
+    print(message)
+
+    tts = gTTS(text=message, lang="ja")
+    tts.save(fn)
+
 
 @st.cache_data
 def load_data():
@@ -42,9 +97,12 @@ if selected_date:
     chois = selected_date.strftime(r"%Y-%m-%d")
 
     if chois in option:
+        fn = pathlib.Path(f"{chois}.mp3")
+
+        if not fn.exists():
+            make_voice(data[chois], fn)
+
         st.subheader(data[chois]["date_week"])
-        audio_url = f"{base_url}/mp3/{chois}.mp3"
-        st.audio(audio_url, format="audio/mpeg")
 
         df = pd.DataFrame(data[chois]["hospitals"]).reindex(
             columns=["name", "medical", "time", "daytime", "address", "lat", "lon"]
@@ -62,6 +120,11 @@ if selected_date:
             use_container_width=True,
             hide_index=True,
         )
+
+        if fn.exists():
+            st.audio(str(fn), format="audio/mpeg")
+        else:
+            st.write("音声データが見つかりません")
 
         m = folium.Map(
             location=[df["lat"].mean(), df["lon"].mean()],
